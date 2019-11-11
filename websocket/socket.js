f7b1d92c1fb12d694573db94426be5f001643c56
Ximelly <ximelly@163.com>
2019-11-11 23:12:00 +0800

    socket应用
    socket应用

--- websocket/socket.js
+++ websocket/socket.js
@@ -6,11 +6,22 @@ let server=http.createServer(()=>{
 })
 server.listen(8080);
 let wx=io.listen(server);
-wx.on("connection",(socket)=>{
+
+let txt="",
+    sockets=[];//所有链接的socket集合
+
+wx.on("connection",socket=>{
     console.log("链接上了");
+    sockets.push(socket);//每次连接上都往sockets里面添加
+    socket.emit("change",txt);//有新用户中途加进来的话初始化数据
+    socket.on("disconnection",socket=>{
+        sockets=sockets.filter(socket=>socket);
+    });
     socket.on("data",data=>{
-        console.log(data);
-        socket.emit("change","服务器的数据改变啦");
+        txt=data;
+        sockets.forEach(socket=>{
+            socket.emit("change",txt);
+        })
     });
 })
 
