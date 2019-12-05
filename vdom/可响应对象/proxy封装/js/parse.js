7aeac3fcd8cda2b5a1a1e17d37091462bbf03a19
Ximelly <ximelly@163.com>
2019-12-05 23:40:39 +0800

    tagName & type
    tagName & type

--- vdom/可响应对象/proxy封装/js/parse.js
+++ vdom/可响应对象/proxy封装/js/parse.js
@@ -2,10 +2,8 @@ function parse(dom){
     assert(dom,"obj cannot be empty");
     assert(dom instanceof Node,"dom must be Node");
 
-    let tagName=dom.tagName;
-
     if(dom.nodeType==document.ELEMENT_NODE){
-        
+        let tagName=dom.tagName.toLowerCase();
         //attribute
         let attrs={};
         Array.from(dom.attributes).forEach(attr=>{
@@ -15,21 +13,26 @@ function parse(dom){
         //children
         //使用map更简单 不必forEach+push
         let children=Array.from(dom.childNodes).map(child=>parse(child)).filter(child=>child!==undefined);
+        //添加tag用来区分<text>slajaslgjk</text> & slajaslgjk
         return {
+            type:"element",
             tagName:tagName,
             attrs:attrs,
-            children:children
+            children:children,
+            //<tag-search>：HTMLElement  <tagSearch>：HTMLUnknownElement
+            isHtml:dom.constructor!==HTMLUnknownElement&&dom.constructor!==HTMLElement,
+            constructor:dom.constructor
         }
     }else if(dom.nodeType==document.TEXT_NODE){
         let str=dom.data.trim();
         if(str){
             return {
-                tagName:"text",
+                type:"text",
                 data:dom.data.trim()
             }
         }else{
             return undefined;
         }
     }
-
+    return undefined;//注释标签
 }
